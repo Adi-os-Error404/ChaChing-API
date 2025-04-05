b27a49d964ac69cf699bf0e500a4542badf87821
Aditya Patel <Adityap902021@gmail.com>
2025-04-06 09:01:11 +1000

    create login user dto

---
+++ src/main/java/com/adityapdev/ChaChing_api/dto/LoginUserDto.java
@@ -0,0 +1,23 @@
+package com.adityapdev.ChaChing_api.dto;
+
+import com.adityapdev.ChaChing_api.util.Security;
+
+public class LoginUserDto {
+
+    private String email;
+    private String password;
+
+    public LoginUserDto(String email, String password) {
+        this.email = email;
+        this.password = Security.hashPassword(password);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
