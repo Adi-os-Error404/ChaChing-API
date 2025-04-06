85d22457961075bb8be2e2acd842e52ca2f622db
Aditya Patel <Adityap902021@gmail.com>
2025-04-06 14:00:39 +1000

    Remove User Permission table

--- src/main/java/com/adityapdev/ChaChing_api/repository/UserPermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.adityapdev.ChaChing_api.repository;
-
-import com.adityapdev.ChaChing_api.entity.UserPermission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPermissionRepository extends JpaRepository<UserPermission, Long> {
-}
