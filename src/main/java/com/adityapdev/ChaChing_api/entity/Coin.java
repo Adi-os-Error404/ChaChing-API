04efa2a483eead5be9634924c1eed98eb9019e7f
Aditya Patel <Adityap902021@gmail.com>
2025-04-10 00:01:33 +1000

    Add Coin entity class

---
+++ src/main/java/com/adityapdev/ChaChing_api/entity/Coin.java
@@ -0,0 +1,100 @@
+package com.adityapdev.ChaChing_api.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "coins")
+public class Coin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "coinId", nullable = false, unique = true)
+    private String coinId;
+
+    @Column(name = "symbol", nullable = false, unique = true)
+    private String symbol;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "currentPriceUsd")
+    private BigDecimal currentPrice;
+
+    @Column(name = "marketCap", nullable = false)
+    private BigDecimal marketCap;
+
+    @OneToMany(mappedBy = "coin", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    public Coin() {}
+
+    public Coin(String coinId, String symbol, String name, BigDecimal currentPrice, BigDecimal marketCap) {
+        this.coinId = coinId;
+        this.symbol = symbol;
+        this.name = name;
+        this.currentPrice = currentPrice;
+        this.marketCap = marketCap;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCoinId() {
+        return coinId;
+    }
+
+    public void setCoinId(String coinId) {
+        this.coinId = coinId;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public BigDecimal getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(BigDecimal marketCap) {
+        this.marketCap = marketCap;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+}
