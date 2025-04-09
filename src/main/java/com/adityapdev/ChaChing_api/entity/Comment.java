947935fee3712beef21417e30ca141394d3a6a18
Aditya Patel <Adityap902021@gmail.com>
2025-04-10 00:01:41 +1000

    Add comment entity class

---
+++ src/main/java/com/adityapdev/ChaChing_api/entity/Comment.java
@@ -0,0 +1,87 @@
+package com.adityapdev.ChaChing_api.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "createdOn", nullable = false)
+    private Instant createdOn;
+
+    @Column(name = "editedOn")
+    private Instant editedOn;
+
+    @ManyToOne
+    @JoinColumn(name = "coin_id", nullable = false)
+    private Coin coin;
+
+    public Comment() {}
+
+    public Comment(String title, String content, Coin coin) {
+        this.title = title;
+        this.content = content;
+        this.createdOn = Instant.now();
+        this.editedOn = null;
+        this.coin = coin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Instant createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Instant getEditedOn() {
+        return editedOn;
+    }
+
+    public void setEditedOn(Instant editedOn) {
+        this.editedOn = editedOn;
+    }
+
+    public Coin getCoin() {
+        return coin;
+    }
+
+    public void setCoin(Coin coin) {
+        this.coin = coin;
+    }
+}
