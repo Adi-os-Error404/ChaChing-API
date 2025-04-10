50eef4c0e2d6cdc065909d60a751c6009b32a699
Aditya Patel <Adityap902021@gmail.com>
2025-04-10 21:03:00 +1000

    add mapper for comment & commentDtos

---
+++ src/main/java/com/adityapdev/ChaChing_api/mapper/CommentMapper.java
@@ -0,0 +1,29 @@
+package com.adityapdev.ChaChing_api.mapper;
+
+import com.adityapdev.ChaChing_api.dto.comment.AddCommentDto;
+import com.adityapdev.ChaChing_api.dto.comment.CommentDetailDto;
+import com.adityapdev.ChaChing_api.entity.Coin;
+import com.adityapdev.ChaChing_api.entity.Comment;
+
+public class CommentMapper {
+
+    public static CommentDetailDto mapToCommentDto(Comment comment, String coinId) {
+        return new CommentDetailDto(
+                comment.getId(),
+                comment.getTitle(),
+                comment.getContent(),
+                comment.getCreatedOn(),
+                comment.getEditedOn(),
+                coinId
+        );
+    }
+
+    public static Comment mapToComment(AddCommentDto addCommentDto, Coin coin) {
+        return new Comment(
+                addCommentDto.getTitle(),
+                addCommentDto.getContent(),
+                coin
+        );
+    }
+
+}
