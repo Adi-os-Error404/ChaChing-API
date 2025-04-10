e994ae04cdfb8e373ffe1d313f2e5e8375ae3a23
Aditya Patel <Adityap902021@gmail.com>
2025-04-10 21:00:31 +1000

    Create a contract - interface for Coin services

--- src/main/java/com/adityapdev/ChaChing_api/service/interfaces/ICoinService.java
+++ src/main/java/com/adityapdev/ChaChing_api/service/interfaces/ICoinService.java
@@ -1,4 +1,15 @@
 package com.adityapdev.ChaChing_api.service.interfaces;
 
+import com.adityapdev.ChaChing_api.dto.coin.AddCoinDto;
+import com.adityapdev.ChaChing_api.dto.coin.CoinDetailDto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ICoinService {
+    CoinDetailDto addCoin(AddCoinDto addCoinDto);
+    List<CoinDetailDto> getAllCoins();
+    CoinDetailDto getCoinById(String coinId);
+    CoinDetailDto updateCurrentPrice(String coinId, BigDecimal currentPriceUsd);
+    void deleteCoin(String coinId);
 }
