378463078106940d880826da28d0c548a1f80677
Aditya Patel <Adityap902021@gmail.com>
2025-04-10 21:03:47 +1000

    add findCoinByCoinId method in coinRepo

--- src/main/java/com/adityapdev/ChaChing_api/repository/CoinRepository.java
+++ src/main/java/com/adityapdev/ChaChing_api/repository/CoinRepository.java
@@ -3,5 +3,8 @@ package com.adityapdev.ChaChing_api.repository;
 import com.adityapdev.ChaChing_api.entity.Coin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CoinRepository extends JpaRepository<Coin, Long> {
+    Optional<Coin> findByCoinId(String coinId);
 }
