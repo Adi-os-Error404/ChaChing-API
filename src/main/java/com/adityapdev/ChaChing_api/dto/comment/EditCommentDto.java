7ddd2b5e945d8e6b5e4f5a7b5b0ee21c8442ec7f
Aditya Patel <Adityap902021@gmail.com>
2025-04-11 22:40:23 +1000

    Add Edit comment dto for updates

---
+++ src/main/java/com/adityapdev/ChaChing_api/dto/comment/EditCommentDto.java
@@ -0,0 +1,9 @@
+package com.adityapdev.ChaChing_api.dto.comment;
+
+public class EditCommentDto extends CommentDto{
+
+    protected EditCommentDto(Long id, String title, String content) {
+        super(id, title, content);
+    }
+
+}
