5dd7fe7dea971bb1a5b9dca6030fe09776557c5a
Aditya Patel <Adityap902021@gmail.com>
2025-04-11 22:38:46 +1000

    update coin service

--- src/main/java/com/adityapdev/ChaChing_api/service/interfaces/ICoinService.java
+++ src/main/java/com/adityapdev/ChaChing_api/service/interfaces/ICoinService.java
@@ -2,6 +2,7 @@ package com.adityapdev.ChaChing_api.service.interfaces;
 
 import com.adityapdev.ChaChing_api.dto.coin.AddCoinDto;
 import com.adityapdev.ChaChing_api.dto.coin.CoinDetailDto;
+import com.adityapdev.ChaChing_api.entity.Coin;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -12,4 +13,5 @@ public interface ICoinService {
     CoinDetailDto getCoinById(String coinId);
     CoinDetailDto updateCurrentPrice(String coinId, BigDecimal currentPriceUsd);
     void deleteCoin(String coinId);
+    Coin findCoinById(String coinId);
 }
