57ee4a54043670d65e5577e6ecf1893ecb9ce4ae
Aditya Patel <Adityap902021@gmail.com>
2025-04-11 22:40:11 +1000

    Update comment dtos

--- src/main/java/com/adityapdev/ChaChing_api/dto/comment/AddCommentDto.java
+++ src/main/java/com/adityapdev/ChaChing_api/dto/comment/AddCommentDto.java
@@ -3,9 +3,8 @@ package com.adityapdev.ChaChing_api.dto.comment;
 import java.time.Instant;
 
 public class AddCommentDto extends CommentDto{
-
-    protected AddCommentDto(Long id, String title, String content, Instant createdOn, Instant editedOn, String coinId) {
-        super(id, title, content, createdOn, editedOn, coinId);
+    public AddCommentDto(String title, String content) {
+        super(title, content);
     }
 
 }
--- src/main/java/com/adityapdev/ChaChing_api/dto/comment/CommentDetailDto.java
+++ src/main/java/com/adityapdev/ChaChing_api/dto/comment/CommentDetailDto.java
@@ -3,7 +3,21 @@ package com.adityapdev.ChaChing_api.dto.comment;
 import java.time.Instant;
 
 public class CommentDetailDto extends CommentDto{
-    public CommentDetailDto(Long id, String title, String content, Instant createdOn, Instant editedOn, String coinId) {
-        super(id, title, content, createdOn, editedOn, coinId);
+    private Instant createdOn;
+    private Instant editedOn;
+
+    public CommentDetailDto(Long id, String title, String content, Instant createdOn, Instant editedOn) {
+        super(id, title, content);
+        this.createdOn = createdOn;
+        this.editedOn = editedOn;
     }
+
+    public Instant getCreatedOn() {
+        return createdOn;
+    }
+
+    public Instant getEditedOn() {
+        return editedOn;
+    }
+
 }
