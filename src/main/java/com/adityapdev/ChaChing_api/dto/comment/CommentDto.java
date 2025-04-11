033c9cd614478c996cede710bd8fe240561759c0
Aditya Patel <Adityap902021@gmail.com>
2025-04-11 22:39:52 +1000

    remove not used getters and setters of comment dto, update constructors

--- src/main/java/com/adityapdev/ChaChing_api/dto/comment/CommentDto.java
+++ src/main/java/com/adityapdev/ChaChing_api/dto/comment/CommentDto.java
@@ -6,17 +6,15 @@ public abstract class CommentDto {
     private Long id;
     private String title;
     private String content;
-    private Instant createdOn;
-    private Instant editedOn;
-    private String coinId;
 
-    protected CommentDto(Long id, String title, String content, Instant createdOn, Instant editedOn, String coinId) {
+    protected CommentDto(Long id, String title, String content) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.createdOn = createdOn;
-        this.editedOn = editedOn;
-        this.coinId = coinId;
+    }
+    protected CommentDto(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 
     public Long getId() {
@@ -31,39 +29,8 @@ public abstract class CommentDto {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Instant getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(Instant createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public Instant getEditedOn() {
-        return editedOn;
-    }
-
-    public void setEditedOn(Instant editedOn) {
-        this.editedOn = editedOn;
-    }
-
-    public String getCoinId() {
-        return coinId;
-    }
-
-    public void setCoinId(String coinId) {
-        this.coinId = coinId;
-    }
 }
