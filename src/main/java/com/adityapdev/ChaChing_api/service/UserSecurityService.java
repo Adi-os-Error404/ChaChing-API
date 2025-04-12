474408704d006bb5b595de80c43d84db5e1470f9
Aditya Patel <Adityap902021@gmail.com>
2025-04-12 16:59:46 +1000

    add an implementation for UserDetailService - find user by username

---
+++ src/main/java/com/adityapdev/ChaChing_api/service/UserSecurityService.java
@@ -0,0 +1,26 @@
+package com.adityapdev.ChaChing_api.service;
+
+import com.adityapdev.ChaChing_api.entity.User;
+import com.adityapdev.ChaChing_api.entity.UserPrincipal;
+import com.adityapdev.ChaChing_api.exception.ResourceNotFoundException;
+import com.adityapdev.ChaChing_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User \"%s\" does not exist", username)));
+        return new UserPrincipal(user);
+    }
+
+}
