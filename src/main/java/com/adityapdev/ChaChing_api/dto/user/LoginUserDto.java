26991602b26d6559d7dc076378c006cf62a64155
Aditya Patel <Adityap902021@gmail.com>
2025-04-12 17:41:42 +1000

    change login user dto - email to username

--- src/main/java/com/adityapdev/ChaChing_api/dto/user/LoginUserDto.java
+++ src/main/java/com/adityapdev/ChaChing_api/dto/user/LoginUserDto.java
@@ -1,19 +1,17 @@
 package com.adityapdev.ChaChing_api.dto.user;
 
-import com.adityapdev.ChaChing_api.util.Security;
-
 public class LoginUserDto {
 
-    private String email;
+    private String username;
     private String password;
 
-    public LoginUserDto(String email, String password) {
-        this.email = email;
-        this.password = Security.hashPassword(password);
+    public LoginUserDto(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public String getEmail() {
-        return email;
+    public String getUsername() {
+        return username;
     }
 
     public String getPassword() {
