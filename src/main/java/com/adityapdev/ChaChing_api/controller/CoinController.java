de4a1e6bbe69a3c54a96468edbd7285224985fed
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 16:45:07 +1000

    Remove put and add Coin apis - use new dto

--- src/main/java/com/adityapdev/ChaChing_api/controller/CoinController.java
+++ src/main/java/com/adityapdev/ChaChing_api/controller/CoinController.java
@@ -1,13 +1,11 @@
 package com.adityapdev.ChaChing_api.controller;
 
-import com.adityapdev.ChaChing_api.dto.coin.AddCoinDto;
-import com.adityapdev.ChaChing_api.dto.coin.CoinCommentDetailDto;
+import com.adityapdev.ChaChing_api.dto.coin.CoinAllDetailsDto;
 import com.adityapdev.ChaChing_api.service.interfaces.ICoinService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -20,28 +18,16 @@ public class CoinController {
         this.coinService = coinService;
     }
 
-    @PostMapping
-    public ResponseEntity<CoinCommentDetailDto> addCoin(@RequestBody AddCoinDto addCoinDto) {
-        CoinCommentDetailDto createdCoin = coinService.addCoin(addCoinDto);
-        return new ResponseEntity<>(createdCoin, HttpStatus.CREATED);
-    }
-
     @GetMapping
-    public ResponseEntity<List<CoinCommentDetailDto>> getAllCoins() {
-        List<CoinCommentDetailDto> coins = coinService.getAllCoins();
+    public ResponseEntity<List<CoinAllDetailsDto>> getAllCoins() {
+        List<CoinAllDetailsDto> coins = coinService.getAllCoins();
         return new ResponseEntity<>(coins, HttpStatus.OK);
     }
 
     @GetMapping("/{coinId}")
-    public ResponseEntity<CoinCommentDetailDto> getCoinById(@PathVariable String coinId) {
-        CoinCommentDetailDto coin = coinService.getCoinById(coinId);
+    public ResponseEntity<CoinAllDetailsDto> getCoinById(@PathVariable String coinId) {
+        CoinAllDetailsDto coin = coinService.getCoinById(coinId);
         return new ResponseEntity<>(coin, HttpStatus.OK);
-    }
-
-    @PutMapping("/{coinId}/price")
-    public ResponseEntity<CoinCommentDetailDto> updateCurrentPrice(@PathVariable String coinId, @RequestBody BigDecimal currentPriceUsd) {
-        CoinCommentDetailDto updatedCoin = coinService.updateCurrentPrice(coinId, currentPriceUsd);
-        return new ResponseEntity<>(updatedCoin, HttpStatus.OK);
     }
 
     @DeleteMapping("/{coinId}")
