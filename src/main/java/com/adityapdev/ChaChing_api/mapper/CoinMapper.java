fa412a7989e06f925ab2720ffcf3f2e0cf2f5ff5
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 15:43:43 +1000

    use the updated dto in coin mapper

--- src/main/java/com/adityapdev/ChaChing_api/mapper/CoinMapper.java
+++ src/main/java/com/adityapdev/ChaChing_api/mapper/CoinMapper.java
@@ -1,7 +1,7 @@
 package com.adityapdev.ChaChing_api.mapper;
 
 import com.adityapdev.ChaChing_api.dto.coin.AddCoinDto;
-import com.adityapdev.ChaChing_api.dto.coin.CoinDetailDto;
+import com.adityapdev.ChaChing_api.dto.coin.CoinCommentDetailDto;
 import com.adityapdev.ChaChing_api.dto.comment.CommentDetailDto;
 import com.adityapdev.ChaChing_api.entity.Coin;
 
@@ -10,12 +10,12 @@ import java.util.stream.Collectors;
 
 public class CoinMapper {
 
-    public static CoinDetailDto mapToCoinDto(Coin coin) {
+    public static CoinCommentDetailDto mapToCoinDto(Coin coin) {
         List<CommentDetailDto> commentDtos = coin.getComments().stream()
                 .map(CommentMapper::mapToCommentDto)
                 .collect(Collectors.toList());
 
-        return new CoinDetailDto(
+        return new CoinCommentDetailDto(
                 coin.getCoinId(),
                 coin.getSymbol(),
                 coin.getName(),
