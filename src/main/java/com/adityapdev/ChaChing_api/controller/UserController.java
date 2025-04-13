8cd6c995cb7b68b57e7dfd947455e75aed64bea5
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 13:24:29 +1000

    Update user controller - change api names and use new services

--- src/main/java/com/adityapdev/ChaChing_api/controller/UserController.java
+++ src/main/java/com/adityapdev/ChaChing_api/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.adityapdev.ChaChing_api.controller;
 
 
-import com.adityapdev.ChaChing_api.dto.user.LoginUserDto;
-import com.adityapdev.ChaChing_api.dto.user.RegisterNewUserDto;
-import com.adityapdev.ChaChing_api.dto.user.UpdateUserPassDto;
-import com.adityapdev.ChaChing_api.dto.user.UserDetailDto;
+import com.adityapdev.ChaChing_api.dto.user.*;
 import com.adityapdev.ChaChing_api.service.interfaces.IUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,40 +32,29 @@ public class UserController {
         return ResponseEntity.ok(res);
     }
 
-    @GetMapping
-    public ResponseEntity<List<UserDetailDto>> getAllUsers() {
-        List<UserDetailDto> users = userService.getAllUsers();
-        return ResponseEntity.ok(users);
-    }
-
-    @GetMapping("{email}")
-    public ResponseEntity<UserDetailDto> getUserByEmail(@PathVariable("email") String email) {
-        UserDetailDto userDto = userService.getUserByEmail(email);
+    @GetMapping("/account")
+    public ResponseEntity<UserDetailDto> getUserDetails() {
+        UserDetailDto userDto = userService.getLoggedInUser();
         return ResponseEntity.ok(userDto);
     }
 
-    @PutMapping("{id}/name")
-    public ResponseEntity<UserDetailDto> updateUserFirstLastName(@PathVariable("id") Long id, @RequestBody UserDetailDto userDetailDto) {
-        UserDetailDto userDto = userService.updateUserName(id, userDetailDto);
+    @PutMapping("/account/name")
+    public ResponseEntity<UserDetailDto> updateUserFirstLastName(@RequestBody UpdateUserNamesDto updateUserNamesDto) {
+        UserDetailDto userDto = userService.updateUserFirstLastName(updateUserNamesDto);
         return ResponseEntity.ok(userDto);
     }
 
-    @PutMapping("{id}/permission")
-    public ResponseEntity<UserDetailDto> updateUserPermission(@PathVariable("id") Long id, @RequestBody UserDetailDto userDetailDto) {
-        UserDetailDto userDto = userService.updateUserPermission(id, userDetailDto);
+
+    @PutMapping("/account/password")
+    public ResponseEntity<UserDetailDto> updateUserPassword(@RequestBody UpdateUserPassDto updatedUserDto) {
+        UserDetailDto userDto = userService.updateUserPassword(updatedUserDto);
         return ResponseEntity.ok(userDto);
     }
 
-    @PutMapping("{id}/password")
-    public ResponseEntity<UserDetailDto> updateUserPassword(@PathVariable("id") Long id, @RequestBody UpdateUserPassDto updatedUserDto) {
-        UserDetailDto userDto = userService.updateUserPassword(id, updatedUserDto);
-        return ResponseEntity.ok(userDto);
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
-        return ResponseEntity.ok(String.format("User with id %d is deleted successfully.", id));
+    @DeleteMapping("/account")
+    public ResponseEntity<String> deleteUser(@RequestBody LoginUserDto loginUserDto) {
+        userService.deleteUser(loginUserDto);
+        return ResponseEntity.ok("User deleted successfully.");
     }
 
 }
