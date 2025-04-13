eab14c9582d34dc26b1015d1948469bde68d63c6
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 15:44:30 +1000

    Add Portfolio service impl

---
+++ src/main/java/com/adityapdev/ChaChing_api/service/PortfolioService.java
@@ -0,0 +1,48 @@
+package com.adityapdev.ChaChing_api.service;
+
+import com.adityapdev.ChaChing_api.dto.portfolio.CoinInPortfolioDto;
+import com.adityapdev.ChaChing_api.entity.Coin;
+import com.adityapdev.ChaChing_api.entity.User;
+import com.adityapdev.ChaChing_api.entity.UserCoin;
+import com.adityapdev.ChaChing_api.mapper.PortfolioMapper;
+import com.adityapdev.ChaChing_api.repository.CoinRepository;
+import com.adityapdev.ChaChing_api.repository.UserCoinRepository;
+import com.adityapdev.ChaChing_api.repository.UserRepository;
+import com.adityapdev.ChaChing_api.service.interfaces.ICoinService;
+import com.adityapdev.ChaChing_api.service.interfaces.IPortfolioService;
+import com.adityapdev.ChaChing_api.service.interfaces.IUserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PortfolioService implements IPortfolioService {
+
+    private final UserCoinRepository userCoinRepository;
+    private final IUserService userService;
+    private final ICoinService coinService;
+
+    public PortfolioService(UserCoinRepository userCoinRepository, UserRepository userRepository, CoinRepository coinRepository) {
+        this.userCoinRepository = userCoinRepository;
+        this.userService = new UserService(userRepository);
+        this.coinService = new CoinService(coinRepository);
+    }
+
+    @Override
+    public List<CoinInPortfolioDto> getCurrentUserCoins() {
+        List<Coin> userCoins = userCoinRepository.findCoinsByUserId(userService.getCurrentUser().getId());
+        return userCoins.stream().map(PortfolioMapper::mapToPortfolioDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public CoinInPortfolioDto addCoinToUserPort(String coinId) {
+        User user = userService.getCurrentUser();
+        Coin coin = coinService.findCoinById(coinId);
+        UserCoin userCoin = new UserCoin(user, coin);
+        userCoinRepository.save(userCoin);
+        return PortfolioMapper.mapToPortfolioDto(coin);
+    }
+
+
+}
