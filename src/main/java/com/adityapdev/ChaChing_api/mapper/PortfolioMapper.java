0277fabee304320ace8b1b11f6c9ee2dcb423926
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 15:44:47 +1000

    Add portfolio mapper from coin to dto

---
+++ src/main/java/com/adityapdev/ChaChing_api/mapper/PortfolioMapper.java
@@ -0,0 +1,18 @@
+package com.adityapdev.ChaChing_api.mapper;
+
+import com.adityapdev.ChaChing_api.dto.portfolio.CoinInPortfolioDto;
+import com.adityapdev.ChaChing_api.entity.Coin;
+
+public class PortfolioMapper {
+
+    public static CoinInPortfolioDto mapToPortfolioDto(Coin coin) {
+        return new CoinInPortfolioDto(
+                coin.getCoinId(),
+                coin.getSymbol(),
+                coin.getName(),
+                coin.getCurrentPrice(),
+                coin.getMarketCap()
+        );
+    }
+
+}
