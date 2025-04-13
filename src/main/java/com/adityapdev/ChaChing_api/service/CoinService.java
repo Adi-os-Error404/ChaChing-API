f06d28d43344406aa2c3530e828472a1444ebee5
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 15:42:14 +1000

    Update dtos in coin services

--- src/main/java/com/adityapdev/ChaChing_api/service/CoinService.java
+++ src/main/java/com/adityapdev/ChaChing_api/service/CoinService.java
@@ -1,12 +1,13 @@
 package com.adityapdev.ChaChing_api.service;
 
 import com.adityapdev.ChaChing_api.dto.coin.AddCoinDto;
-import com.adityapdev.ChaChing_api.dto.coin.CoinDetailDto;
+import com.adityapdev.ChaChing_api.dto.coin.CoinCommentDetailDto;
 import com.adityapdev.ChaChing_api.entity.Coin;
 import com.adityapdev.ChaChing_api.exception.ConflictException;
 import com.adityapdev.ChaChing_api.exception.ResourceNotFoundException;
 import com.adityapdev.ChaChing_api.mapper.CoinMapper;
 import com.adityapdev.ChaChing_api.repository.CoinRepository;
+import com.adityapdev.ChaChing_api.service.interfaces.ICoinGeckoService;
 import com.adityapdev.ChaChing_api.service.interfaces.ICoinService;
 import org.springframework.stereotype.Service;
 
@@ -19,13 +20,14 @@ import java.util.stream.Collectors;
 public class CoinService implements ICoinService {
 
     private final CoinRepository coinRepository;
+    private final ICoinGeckoService coinGeckoService = new CoinGeckoService();
 
     public CoinService(CoinRepository coinRepository) {
         this.coinRepository = coinRepository;
     }
 
     @Override
-    public CoinDetailDto addCoin(AddCoinDto addCoinDto) {
+    public CoinCommentDetailDto addCoin(AddCoinDto addCoinDto) {
         if (coinRepository.findByCoinId(addCoinDto.getCoinId()).isPresent())
             throw new ConflictException(String.format("Coin \"%s\" is already exists.", addCoinDto.getCoinId()));
         Coin coin = CoinMapper.mapToCoin(addCoinDto);
@@ -34,18 +36,18 @@ public class CoinService implements ICoinService {
     }
 
     @Override
-    public List<CoinDetailDto> getAllCoins() {
+    public List<CoinCommentDetailDto> getAllCoins() {
         List<Coin> coins = coinRepository.findAll();
         return coins.stream().map(CoinMapper::mapToCoinDto).collect(Collectors.toList());
     }
 
     @Override
-    public CoinDetailDto getCoinById(String coinId) {
+    public CoinCommentDetailDto getCoinById(String coinId) {
         return CoinMapper.mapToCoinDto(findCoinById(coinId));
     }
 
     @Override
-    public CoinDetailDto updateCurrentPrice(String coinId, BigDecimal currentPriceUsd) {
+    public CoinCommentDetailDto updateCurrentPrice(String coinId, BigDecimal currentPriceUsd) {
         Coin coin = findCoinById(coinId);
         coin.setCurrentPrice(currentPriceUsd);
         Coin savedCoin = coinRepository.save(coin);
@@ -54,14 +56,19 @@ public class CoinService implements ICoinService {
 
     @Override
     public void deleteCoin(String coinId) {
-        Coin coin = findCoinById(coinId);
+        Coin coin = coinRepository.findByCoinId(coinId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Coin \"%s\" is not found.", coinId)));
         coinRepository.deleteById(coin.getId());
     }
 
     @Override
     public Coin findCoinById(String coinId) {
-        return coinRepository.findByCoinId(coinId)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Coin \"%s\" is not found.", coinId)));
+        Optional<Coin> coinInDb = coinRepository.findByCoinId(coinId);
+        if (coinInDb.isEmpty()) {
+            Coin newCoin = coinGeckoService.getCoinByCoinId(coinId);
+            return coinRepository.save(newCoin);
+        }
+        return coinInDb.get();
     }
 
 }
--- src/main/java/com/adityapdev/ChaChing_api/service/interfaces/ICoinService.java
+++ src/main/java/com/adityapdev/ChaChing_api/service/interfaces/ICoinService.java
@@ -1,17 +1,17 @@
 package com.adityapdev.ChaChing_api.service.interfaces;
 
 import com.adityapdev.ChaChing_api.dto.coin.AddCoinDto;
-import com.adityapdev.ChaChing_api.dto.coin.CoinDetailDto;
+import com.adityapdev.ChaChing_api.dto.coin.CoinCommentDetailDto;
 import com.adityapdev.ChaChing_api.entity.Coin;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface ICoinService {
-    CoinDetailDto addCoin(AddCoinDto addCoinDto);
-    List<CoinDetailDto> getAllCoins();
-    CoinDetailDto getCoinById(String coinId);
-    CoinDetailDto updateCurrentPrice(String coinId, BigDecimal currentPriceUsd);
+    CoinCommentDetailDto addCoin(AddCoinDto addCoinDto);
+    List<CoinCommentDetailDto> getAllCoins();
+    CoinCommentDetailDto getCoinById(String coinId);
+    CoinCommentDetailDto updateCurrentPrice(String coinId, BigDecimal currentPriceUsd);
     void deleteCoin(String coinId);
     Coin findCoinById(String coinId);
 }
