0e32a932b718451eef63e82c2fe847e2c2c0b9a1
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 15:44:59 +1000

    Add portfolio controller - define apis

---
+++ src/main/java/com/adityapdev/ChaChing_api/controller/PortfolioController.java
@@ -0,0 +1,33 @@
+package com.adityapdev.ChaChing_api.controller;
+
+import com.adityapdev.ChaChing_api.dto.portfolio.CoinInPortfolioDto;
+import com.adityapdev.ChaChing_api.service.interfaces.IPortfolioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/portfolio")
+public class PortfolioController {
+
+    private IPortfolioService portfolioService;
+
+    public PortfolioController(IPortfolioService portfolioService) {
+        this.portfolioService = portfolioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CoinInPortfolioDto>> getPortfolio() {
+        List<CoinInPortfolioDto> userCoins = portfolioService.getCurrentUserCoins();
+        return new ResponseEntity<>(userCoins, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<CoinInPortfolioDto> addCoinToPortfolio(@RequestParam String coinId) {
+        CoinInPortfolioDto addedCoin = portfolioService.addCoinToUserPort(coinId);
+        return new ResponseEntity<>(addedCoin, HttpStatus.OK);
+    }
+
+}
