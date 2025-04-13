500dbb1ea2b19d60aaf9f4c988cc9e4519128a88
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 13:24:12 +1000

    Add Update Names Dto for user

---
+++ src/main/java/com/adityapdev/ChaChing_api/dto/user/UpdateUserNamesDto.java
@@ -0,0 +1,28 @@
+package com.adityapdev.ChaChing_api.dto.user;
+
+public class UpdateUserNamesDto
+{
+    private String firstName;
+    private String lastName;
+
+    public UpdateUserNamesDto(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
