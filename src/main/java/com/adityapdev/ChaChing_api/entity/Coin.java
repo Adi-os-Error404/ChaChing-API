4455dc9fc6d105649778f4bb57217bf1880c1ff2
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 16:46:07 +1000

    Update coin entity to include all details from the api

--- src/main/java/com/adityapdev/ChaChing_api/entity/Coin.java
+++ src/main/java/com/adityapdev/ChaChing_api/entity/Coin.java
@@ -28,19 +28,89 @@ public class Coin {
     @Column(name = "market_cap", nullable = false)
     private Long marketCap;
 
+    @Column(name = "market_cap_rank")
+    private Integer marketCapRank;
+
+    @Column(name = "genesis_date")
+    private String genesisDate;
+
+    @Column(name = "sentiment_votes_up_percentage")
+    private BigDecimal sentimentVotesUpPercentage;
+
+    @Column(name = "sentiment_votes_down_percentage")
+    private BigDecimal sentimentVotesDownPercentage;
+
+    @Column(name = "watchlist_portfolio_users")
+    private Long watchlistPortfolioUsers;
+
+    @Column(name = "image_thumb")
+    private String imageThumb;
+
+    @Column(name = "image_small")
+    private String imageSmall;
+
+    @Column(name = "image_large")
+    private String imageLarge;
+
+    // New fields
+    @Column(name = "ath_usd")
+    private BigDecimal ath;
+
+    @Column(name = "atl_usd")
+    private BigDecimal atl;
+
+    @Column(name = "high_24h_usd")
+    private BigDecimal high24h;
+
+    @Column(name = "low_24h_usd")
+    private BigDecimal low24h;
+
+    @Column(name = "total_supply")
+    private BigDecimal totalSupply;
+
+    @Column(name = "max_supply")
+    private BigDecimal maxSupply;
+
+    @Column(name = "circulating_supply")
+    private BigDecimal circulatingSupply;
+
     @OneToMany(mappedBy = "coin", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
 
-    public Coin() {}
+    public Coin() {
+        this.comments = new ArrayList<>();
+    }
 
-    public Coin(String coinId, String symbol, String name, BigDecimal currentPrice, Long marketCap) {
+    public Coin(String coinId, String symbol, String name, BigDecimal currentPrice, Long marketCap,
+                Integer marketCapRank, String genesisDate, BigDecimal sentimentVotesUpPercentage,
+                BigDecimal sentimentVotesDownPercentage, Long watchlistPortfolioUsers,
+                String imageThumb, String imageSmall, String imageLarge, BigDecimal ath, BigDecimal atl,
+                BigDecimal high24h, BigDecimal low24h, BigDecimal totalSupply, BigDecimal maxSupply,
+                BigDecimal circulatingSupply) {
         this.coinId = coinId;
         this.symbol = symbol;
         this.name = name;
         this.currentPrice = currentPrice;
         this.marketCap = marketCap;
+        this.marketCapRank = marketCapRank;
+        this.genesisDate = genesisDate;
+        this.sentimentVotesUpPercentage = sentimentVotesUpPercentage;
+        this.sentimentVotesDownPercentage = sentimentVotesDownPercentage;
+        this.watchlistPortfolioUsers = watchlistPortfolioUsers;
+        this.imageThumb = imageThumb;
+        this.imageSmall = imageSmall;
+        this.imageLarge = imageLarge;
+        this.ath = ath;
+        this.atl = atl;
+        this.high24h = high24h;
+        this.low24h = low24h;
+        this.totalSupply = totalSupply;
+        this.maxSupply = maxSupply;
+        this.circulatingSupply = circulatingSupply;
         this.comments = new ArrayList<>();
     }
+
+    // Getters and Setters
 
     public Long getId() {
         return id;
@@ -74,6 +144,130 @@ public class Coin {
         return marketCap;
     }
 
+    public void setMarketCap(Long marketCap) {
+        this.marketCap = marketCap;
+    }
+
+    public Integer getMarketCapRank() {
+        return marketCapRank;
+    }
+
+    public void setMarketCapRank(Integer marketCapRank) {
+        this.marketCapRank = marketCapRank;
+    }
+
+    public String getGenesisDate() {
+        return genesisDate;
+    }
+
+    public void setGenesisDate(String genesisDate) {
+        this.genesisDate = genesisDate;
+    }
+
+    public BigDecimal getSentimentVotesUpPercentage() {
+        return sentimentVotesUpPercentage;
+    }
+
+    public void setSentimentVotesUpPercentage(BigDecimal sentimentVotesUpPercentage) {
+        this.sentimentVotesUpPercentage = sentimentVotesUpPercentage;
+    }
+
+    public BigDecimal getSentimentVotesDownPercentage() {
+        return sentimentVotesDownPercentage;
+    }
+
+    public void setSentimentVotesDownPercentage(BigDecimal sentimentVotesDownPercentage) {
+        this.sentimentVotesDownPercentage = sentimentVotesDownPercentage;
+    }
+
+    public Long getWatchlistPortfolioUsers() {
+        return watchlistPortfolioUsers;
+    }
+
+    public void setWatchlistPortfolioUsers(Long watchlistPortfolioUsers) {
+        this.watchlistPortfolioUsers = watchlistPortfolioUsers;
+    }
+
+    public String getImageThumb() {
+        return imageThumb;
+    }
+
+    public void setImageThumb(String imageThumb) {
+        this.imageThumb = imageThumb;
+    }
+
+    public String getImageSmall() {
+        return imageSmall;
+    }
+
+    public void setImageSmall(String imageSmall) {
+        this.imageSmall = imageSmall;
+    }
+
+    public String getImageLarge() {
+        return imageLarge;
+    }
+
+    public void setImageLarge(String imageLarge) {
+        this.imageLarge = imageLarge;
+    }
+
+    public BigDecimal getAth() {
+        return ath;
+    }
+
+    public void setAth(BigDecimal ath) {
+        this.ath = ath;
+    }
+
+    public BigDecimal getAtl() {
+        return atl;
+    }
+
+    public void setAtl(BigDecimal atl) {
+        this.atl = atl;
+    }
+
+    public BigDecimal getHigh24h() {
+        return high24h;
+    }
+
+    public void setHigh24h(BigDecimal high24h) {
+        this.high24h = high24h;
+    }
+
+    public BigDecimal getLow24h() {
+        return low24h;
+    }
+
+    public void setLow24h(BigDecimal low24h) {
+        this.low24h = low24h;
+    }
+
+    public BigDecimal getTotalSupply() {
+        return totalSupply;
+    }
+
+    public void setTotalSupply(BigDecimal totalSupply) {
+        this.totalSupply = totalSupply;
+    }
+
+    public BigDecimal getMaxSupply() {
+        return maxSupply;
+    }
+
+    public void setMaxSupply(BigDecimal maxSupply) {
+        this.maxSupply = maxSupply;
+    }
+
+    public BigDecimal getCirculatingSupply() {
+        return circulatingSupply;
+    }
+
+    public void setCirculatingSupply(BigDecimal circulatingSupply) {
+        this.circulatingSupply = circulatingSupply;
+    }
+
     public List<Comment> getComments() {
         return comments;
     }
@@ -81,5 +275,4 @@ public class Coin {
     public void addComment(Comment comment) {
         this.comments.add(comment);
     }
-
 }
