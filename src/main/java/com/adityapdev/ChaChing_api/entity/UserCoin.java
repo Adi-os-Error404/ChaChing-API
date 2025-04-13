11636a0bda760a23f27cc6f2ccb214af4d9a42c6
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 13:51:41 +1000

    Add a linking table for user and coins - manage portfolio

---
+++ src/main/java/com/adityapdev/ChaChing_api/entity/UserCoin.java
@@ -0,0 +1,49 @@
+package com.adityapdev.ChaChing_api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_coin")
+public class UserCoin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "coin_id", nullable = false)
+    private Coin coin;
+
+    public UserCoin() {}
+
+    public UserCoin(User user, Coin coin) {
+        this.user = user;
+        this.coin = coin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Coin getCoin() {
+        return coin;
+    }
+
+    public void setCoin(Coin coin) {
+        this.coin = coin;
+    }
+
+}
