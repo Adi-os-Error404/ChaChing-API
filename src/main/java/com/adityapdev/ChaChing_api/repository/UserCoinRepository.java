c42ad14d52c201f6ea628905c6bec63c8740788c
Aditya Patel <Adityap902021@gmail.com>
2025-04-13 15:44:09 +1000

    Add UserCoin repo to talk to UserCoin table

---
+++ src/main/java/com/adityapdev/ChaChing_api/repository/UserCoinRepository.java
@@ -0,0 +1,16 @@
+package com.adityapdev.ChaChing_api.repository;
+
+import com.adityapdev.ChaChing_api.entity.Coin;
+import com.adityapdev.ChaChing_api.entity.UserCoin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserCoinRepository extends JpaRepository<UserCoin, Long> {
+
+    @Query("SELECT uc.coin FROM UserCoin uc WHERE uc.user.id = :userId")
+    List<Coin> findCoinsByUserId(@Param("userId") Long userId);
+
+}
