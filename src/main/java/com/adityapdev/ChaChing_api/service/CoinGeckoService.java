2cb5108bbc2fdd121219d2242384360f27202a80
Aditya Patel <Adityap902021@gmail.com>
2025-04-21 15:30:24 +1000

    implement getCurrentPrice coin gecko service

--- src/main/java/com/adityapdev/ChaChing_api/service/CoinGeckoService.java
+++ src/main/java/com/adityapdev/ChaChing_api/service/CoinGeckoService.java
@@ -8,14 +8,18 @@ import com.adityapdev.ChaChing_api.service.interfaces.ICoinGeckoService;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 public class CoinGeckoService implements ICoinGeckoService {
 
     public final static String CURRENCY = "usd";
+    private final static String GECKO_API = "https://api.coingecko.com/api/v3";
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public Coin getCoinByCoinId(String coinId) {
-        String coinDetailsApi = "https://api.coingecko.com/api/v3/coins/" + coinId;
+        String coinDetailsApi = GECKO_API + "/coins/" + coinId;
 
         try {
             CoinGeckoDto details = restTemplate.getForObject(coinDetailsApi, CoinGeckoDto.class);
@@ -24,6 +28,24 @@ public class CoinGeckoService implements ICoinGeckoService {
         } catch (RestClientException ex) {
             throw new ResourceNotFoundException(coinId + " does not exist.");
         }
+    }
+
+    @Override
+    public BigDecimal getCurrentPrice(String coinId) {
+        String currentPriceApi = GECKO_API + "/simple/price?ids=" + coinId + "&vs_currencies=" + CURRENCY;
+        try {
+            Map<String, Map<String, Number>> response = restTemplate.getForObject(currentPriceApi, Map.class);
+
+            if (response != null && response.containsKey(coinId)) {
+                Number priceNumber = response.get(coinId).get(CURRENCY);
+                return new BigDecimal(priceNumber.toString());
+            } else {
+                throw new ResourceNotFoundException(coinId + " price could not be found.");
+            }
+        } catch (RestClientException ex) {
+            throw new ResourceNotFoundException("Error fetching price for " + coinId);
+        }
+
     }
 
 }
